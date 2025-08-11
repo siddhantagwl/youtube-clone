745be44470b2d774989083fe5fc170df62a875c0
Siddhant Agarwal <siddhantagwl29@gmail.com>
2025-08-11 11:52:05 +0200

    add Firebase authentication integration
    - Added Firebase SDK for authentication in filebase.ts
    - Implemented Google sign-in and sign-out functions
    - Added onAuthStateChanged helper for user state management

---
+++ yt-web-client/app/firebase/filebase.ts
@@ -0,0 +1,41 @@
+// config and lib code , not a page as per next JS structure.
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged,User } from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAchRkKq2Zt0BPizNitoErJJ7Aora_e72A",
+  authDomain: "yt-clone-8285c.firebaseapp.com",
+  projectId: "yt-clone-8285c",
+
+
+  appId: "1:410145935945:web:44634b2c7a56d25b53a598",
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+
+// sign in with Google pop up
+export const signInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  const result = signInWithPopup(auth, provider);
+  return result;
+}
+
+//sign out
+export const signOut = () => {
+  return auth.signOut();
+};
+
+// trigger a callback when user auth state changes
+export const onAuthStateChangedHelper = (callback: (user: User | null) => void) => {
+  return onAuthStateChanged(auth, callback);
+};
